c868571a0c21bcbf6b2c25aea97c3611e0be4016
MohammedBasha <me@mohammedbasha.com>
2021-07-28 06:24:34 +0200

    polymorphism

--- index.php
+++ index.php
@@ -1,36 +1,20 @@
 <?php
 
 /*
- * Abstract class:
- * Can't be instantiated = can't create an object from it.
- * Made fot other classes to inherit from it only.
- * Abstracted functions don't have body and can have arguments.
- * visibility markers should be at the same level or less and not higher in the inherited classes.
+ * Polymorphism class:
  *
  * */
 
-abstract class makeDevice {
-    public $ram;
-
-    public function sayHello() {
-        echo 'Say Hello ';
-    }
-
-    abstract public function sayBye($arg);
+interface Mobile {
+    public function pressHome();
 }
 
-// Super class / main class / parent class
-class appleDevice extends makeDevice {
-    public function sayBye($ar) {
-        echo ' ' . $ar;
-    }
+class Iphone implements Mobile {
+    public $name;
+    public function pressHome() {}
 }
 
-$newDevice = new appleDevice();
-$newDevice->sayHello();
-$newDevice->sayBye('h');
-
-echo '<br>';
-echo '<pre>';
-var_dump($newDevice);
-echo '</pre>';
+class Sony implements Mobile {
+    public $name;
+    public function pressHome() {}
+}
